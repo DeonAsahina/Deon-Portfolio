3960aa6d00650ac8c79d462ec1761222c081bfcb
DeonAsahina <160614112+DeonAsahina@users.noreply.github.com>
2026-03-04 23:17:22 +0800

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App'; // Saya hapus ".tsx" di sini agar komputer tidak bingung
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
